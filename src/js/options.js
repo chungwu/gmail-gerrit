d34aba10610c4dc665a9a00883f676828d06697f
Chung Wu <chungwu@gmail.com>
2018-02-15 16:43:37 -0800

    Fix requiring gmail address

--- src/js/options.js
+++ src/js/options.js
@@ -1,51 +1,40 @@
 function save() {
-  var url = _validateUrl($("#url").val());
-  if (!url) { 
+  const url = _validateUrl($("#url").val());
+  if (url === false) {
     return false; 
   }
-  var gmail = _validateEmail($("#gmail").val());
-  if (!gmail) {
+  const gmail = _validateEmail($("#gmail").val());
+  if (gmail === false) {
     return false;
   }
 
-  var context = _validateInt($("#context-lines").val());
+  const context = _validateInt($("#context-lines").val());
 
-  var botNames = $("#bot-names").val();
-
-  // var user = $("#user").val();
-  // var password = $("#password").val();
-  // if (!user || !password) {
-  //   alert("You must enter your authentication info");
-  //   return false;
-  // }
+  const botNames = $("#bot-names").val();
 
   localStorage['host'] = url;
   localStorage['gmail'] = gmail;
   localStorage['contextLines'] = context;
   localStorage['botNames'] = botNames;
   
-  // localStorage['user'] = user;
-  // localStorage['password'] = password;
   _flashMessage("Saved! You should reload your Gmail tabs to reflect the changes.");
   return false;
 }
 
 function _flashMessage(msg) {
-  var $msg = $("<div class='message'/>").text(msg);
-  $msg.prependTo("form.options-form");
-  setTimeout(function() { $msg.fadeOut();}, 5000);
+  $(".message").text(msg).show();
 }
 
 function _validateEmail(email) {
   if (email && email.indexOf("@") < 0) {
-    alert("Invalid email: " + email);
+    _flashMessage(`Invalid email: ${email}`);
     return false;
   }
   return email;
 }
 
 function _validateInt(num) {
-  if (!num || num.length == 0) {
+  if (!num || num.length === 0) {
     return undefined;
   } else {
     return parseInt(num);
@@ -53,12 +42,18 @@ function _validateInt(num) {
 }
 
 function _validateUrl(url) {
-  if (!(url.indexOf("http://") == 0 || url.indexOf("https://") == 0)) {
-    alert("Invalid URL: " + url);
+  // let url = _.trim(url);
+  // console.log("URL", url);
+  if (url.length === 0) {
+    _flashMessage("You must specify your Gerrit URL!");
+    return false;
+  }
+  if (!(url.indexOf("http://") === 0 || url.indexOf("https://") === 0)) {
+   _flashMessage(`Invalid URL; make sure it starts with http:// or https://: ${url}`);
     return false;
   }
 
-  if (url.lastIndexOf("/") == (url.length - 1)) {
+  if (url.lastIndexOf("/") === (url.length - 1)) {
     url = url.substring(0, url.length - 1);
   } 
   return url;
@@ -66,8 +61,6 @@ function _validateUrl(url) {
 
 function load() {
   $("#url").val(localStorage['host']);
-  //$("#user").val(localStorage['user']);
-  //$("#password").val(localStorage['password']);
   $("#gmail").val(localStorage['gmail']);
   $("#context-lines").val(localStorage['contextLines'] || "3");
   $("#bot-names").val(localStorage['botNames'] || "jenkins");
